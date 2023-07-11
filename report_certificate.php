33c466408475fb7811140bc73487372f5961d2ac
israel1996 <40413337+israel1996@users.noreply.github.com>
2023-07-11 00:45:45 -0500

    Certificado completo
    Se finalizó el formato y obtención de datos para el certificado.
    Se agregó un procedimiento para incrementar el número del certificado y reseteo al año.

--- report_certificate.php
+++ report_certificate.php
@@ -32,42 +32,67 @@ class PDF extends FPDF
     }
 
     // Tabla simple
-    function BasicTable($dataEmployee, $dataAdmin)
+    function BasicTable($dataEmployee, $dataAdmin, $rmu)
     {
+        // Array de nombres de meses en español
+        $meses = array(
+            'enero',
+            'febrero',
+            'marzo',
+            'abril',
+            'mayo',
+            'junio',
+            'julio',
+            'agosto',
+            'septiembre',
+            'octubre',
+            'noviembre',
+            'diciembre'
+        );
+        $texto = $dataEmployee['name_departament'];
+        $textoReemplazado = str_replace('Proyecto DAPME', 'Proyecto de Inversión Desarrollo de Agrotecnologías como Estrategias ante la Amenaza de Enfermedades que afecten la Producción de Musáceas en el Ecuador', $texto);
+
+        if ($textoReemplazado === $texto) {
+            // No se encontró la coincidencia, mostrar el contenido original
+            $textoReemplazado = $texto;
+        }
+
+
+        // Obtener la fecha actual en formato español
+        $fecha = date('d') . ' de ' . $meses[date('n') - 1] . ' de ' . date('Y');
         $this->SetFont('Arial', 'B', 11);
-        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "INIAP-UATH-" . date('Y') . "-37-CER"), 0, 1, 'R');
+        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "INIAP-UATH-" . date('Y') . "-". $dataEmployee['countCertificate_numberPeriod'] ."-CER"), 0, 1, 'R');
         $this->SetFont('Arial', '', 10);
-        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Mocache, 09 de junio de 2023"), 0, 1, 'R');
+        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Mocache, " . $fecha), 0, 1, 'R');
         $this->Ln();
-        $this->MultiCell(0, 5, iconv('UTF-8', 'windows-1252', "El o La Ingeniero/a ". $dataAdmin['fullname'] .", Responsable de Administración del Talento Humano, a petición de la interesada"), 0, 'L', 0);
+        $this->MultiCell(0, 5, iconv('UTF-8', 'windows-1252', "Ingeniero/a " . $dataAdmin['fullname_inv2'] . ", Responsable de Administración del Talento Humano, a petición del interesado"), 0, 'L', 0);
         $this->SetFont('Arial', 'B', 11);
         $this->Ln();
         $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "CERTIFICA:"), 0, 1, 'C');
         $this->Ln();
         $this->SetFont('Arial', '', 10);
-        $this->MultiCell(0, 5, iconv('UTF-8', 'windows-1252', "Que LLANOS SALAS MARIA ALEJANDRA con cédula de ciudadanía Nro. 12062470772, labora en esta Institución con la siguiente Información"), 0, 'L', 0);
+        $this->MultiCell(0, 5, iconv('UTF-8', 'windows-1252', "Que " . strtoupper($dataEmployee['fullname']) . " con cédula de ciudadanía Nro." . $dataEmployee['ci_employee'] . ", labora en esta Institución con la siguiente Información:"), 0, 'L', 0);
         $this->Ln(5);
 
         $this->SetFont('Arial', 'B', 10);
         $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Cargo"), 0, 1, 'L');
         $this->SetFont('Arial', '', 10);
-        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Cargo"), 0, 1, 'L');
+        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', $dataEmployee['name_jobTitle']), 0, 1, 'L');
         $this->SetFont('Arial', 'B', 10);
         $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Periodo"), 0, 1, 'L');
         $this->SetFont('Arial', '', 10);
-        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Periodo"), 0, 0, 'L');
-        $this->Cell(2, 5, iconv('UTF-8', 'windows-1252', "Periodo"), 0, 0, 'L');
-        $this->Cell(-150, 5, iconv('UTF-8', 'windows-1252', "Periodo"), 0, 0, 'L');
-        $this->Cell(-150, 5, iconv('UTF-8', 'windows-1252', "Periodo"), 0, 0, 'L');
+        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', $dataEmployee['startDate_employee'] ." - " . date('d/m/Y')), 0, 1, 'L');
         $this->SetFont('Arial', 'B', 10);
         $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Área / Unidad"), 0, 1, 'L');
         $this->SetFont('Arial', '', 10);
-        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Área / Unidad"), 0, 1, 'L');
-        $this->SetFont('Arial', 'B', 10);
-        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Remuneración"), 0, 1, 'L');
-        $this->SetFont('Arial', '', 10);
-        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Remuneración"), 0, 1, 'L');
-
+        $this->MultiCell(0, 5, iconv('UTF-8', 'windows-1252', $textoReemplazado), 0, 'L', 0);
+       
+        if ($rmu) {
+            $this->SetFont('Arial', 'B', 10);
+            $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Remuneración"), 0, 1, 'L');
+            $this->SetFont('Arial', '', 10);
+            $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "$" . $dataEmployee['salary_employee']), 0, 1, 'L');
+        }
         $this->Ln(5);
         $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Tal como consta en los registros de esta Unidad."), 0, 1, 'L');
         $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Es todo en cuanto puedo infromar en honor a la verdad."), 0, 1, 'L');
@@ -75,7 +100,7 @@ class PDF extends FPDF
         $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Atentamente."), 0, 1, 'C');
         $this->Ln(30);
         $this->SetFont('Arial', 'B', 10);
-        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Ing. Carlos Macías Loor"), 0, 1, 'C');
+        $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "Ing. ". $dataAdmin['fullname_inv2']), 0, 1, 'C');
         $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "UNIDAD DE ADMINISTRACIÓN DEL TALENTO HUMANO"), 0, 1, 'C');
         $this->Cell(0, 5, iconv('UTF-8', 'windows-1252', "ESTACIÓN EXPERIMENTAL TROPICAL PICHILINGUE"), 0, 1, 'C');
         $this->Ln(5);
@@ -90,7 +115,7 @@ class PDF extends FPDF
 
         // Definir los datos
         $accion = 'Elaborado por';
-        $nombre = 'Ing. Carlos Macias';
+        $nombre = 'Ing.'. $dataAdmin['fullname_inv'];
         $cargo = 'Responsable de Administracion de Talento Humano';
 
         // Agregar los datos a la tabla
@@ -108,23 +133,30 @@ $rmu = false;
 if (isset($_SESSION['user'])) {
     $user = unserialize($_SESSION['user']);
     $idAdmin = $user->getIdEmployee();
-} 
+}
 
 if (isset($_GET['idEmployee'])) {
     $idEmployee = $_GET['idEmployee'];
 }
 if (isset($_GET['rmu'])) {
-    $rmu = $_GET['rmu'];
+    $rmu = filter_var($_GET['rmu'], FILTER_VALIDATE_BOOLEAN);
 }
+
+$sql_temp = "CALL pa_incrementCountCertificate()";
+$stmt = $conn->prepare($sql_temp);
+$stmt->execute();
 
 $sql = "SELECT 
 id_employee,
 ci_employee,
 fullname,
+fullname_inv,
+fullname_inv2,
 name_jobTitle,
 startDate_employee,
 name_departament,
-salary_employee
+salary_employee,
+countCertificate_numberPeriod
 FROM vw_dataEmloyeeCertificate ";
 
 $sql_employee = $sql . "WHERE id_employee = " . $idEmployee;
@@ -146,7 +178,7 @@ $pdf = new PDF('P', 'mm', 'A4');
 // Carga de datos
 $pdf->SetFont('Arial', '', 10);
 $pdf->AddPage();
-$pdf->BasicTable($dataEmployee, $dataAdmin);
+$pdf->BasicTable($dataEmployee, $dataAdmin, $rmu);
 
 // Generar el archivo PDF
 $pdf->Output('certificado.pdf', 'I');
